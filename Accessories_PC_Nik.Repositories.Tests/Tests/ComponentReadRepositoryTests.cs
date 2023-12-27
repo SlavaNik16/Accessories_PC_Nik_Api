f5e6570f22b76f7e9ff8e2382edf6a6249682404
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-12-27 20:59:13 +0300

    Сделаны теты компонентов и сервисов

---
+++ Accessories_PC_Nik.Repositories.Tests/Tests/ComponentReadRepositoryTests.cs
@@ -0,0 +1,175 @@
+using Accessories_PC_Nik.Context.Tests;
+using Accessories_PC_Nik.Repositories.Contracts.Interface;
+using Accessories_PC_Nik.Repositories.Implementations;
+using FluentAssertions;
+using Xunit;
+
+namespace Accessories_PC_Nik.Repositories.Tests.Tests
+{
+    /// <summary>
+    /// Тесты для <see cref="IComponentsReadRepository"/>
+    /// </summary>
+    public class ComponentReadRepositoryTests : AccessoriesContextInMemory
+    {
+        private readonly IComponentsReadRepository componentsReadRepository;
+
+        public ComponentReadRepositoryTests()
+        {
+            componentsReadRepository = new ComponentsReadRepository(Reader);
+        }
+
+        /// <summary>
+        /// Возвращает пустой список компонентов
+        /// </summary>
+        [Fact]
+        public async Task GetAllComponentEmpty()
+        {
+            // Act
+            var result = await componentsReadRepository.GetAllAsync(CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+        }
+
+        /// <summary>
+        /// Возвращает список компонентов
+        /// </summary>
+        [Fact]
+        public async Task GetAllComponentsValue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Component();
+            await Context.Components.AddRangeAsync(target,
+                TestDataGenerator.Component(x => x.DeletedAt = DateTimeOffset.UtcNow));
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await componentsReadRepository.GetAllAsync(CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.HaveCount(1)
+                .And.ContainSingle(x => x.Id == target.Id);
+        }
+
+        /// <summary>
+        /// Получение компонента по идентификатору и возвращает null
+        /// </summary>
+        [Fact]
+        public async Task GetByIdComponentNull()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await componentsReadRepository.GetByIdAsync(id, CancellationToken);
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Получение компонента по идентификатору и возвращает данные
+        /// </summary>
+        [Fact]
+        public async Task GetByIdComponentValue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Component();
+            await Context.Components.AddAsync(target);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await componentsReadRepository.GetByIdAsync(target.Id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEquivalentTo(target);
+        }
+
+        /// <summary>
+        /// Получение списка компонентов по идентификаторам и возвращает пустую коллекцию
+        /// </summary>
+        [Fact]
+        public async Task GetByIdsComponentsEmpty()
+        {
+            //Arrange
+            var id1 = Guid.NewGuid();
+            var id2 = Guid.NewGuid();
+            var id3 = Guid.NewGuid();
+
+            // Act
+            var result = await componentsReadRepository.GetByIdsAsync(new[] { id1, id2, id3 }, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+        }
+
+        /// <summary>
+        /// Получение списка компонентов по идентификаторам и возвращает данные
+        /// </summary>
+        [Fact]
+        public async Task GetByIdsComponentsValue()
+        {
+            //Arrange
+            var target1 = TestDataGenerator.Component();
+            var target2 = TestDataGenerator.Component(x => x.DeletedAt = DateTimeOffset.UtcNow);
+            var target3 = TestDataGenerator.Component();
+            var target4 = TestDataGenerator.Component();
+            await Context.Components.AddRangeAsync(target1, target2, target3, target4);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await componentsReadRepository.GetByIdsAsync(new[] { target1.Id, target2.Id, target4.Id }, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.HaveCount(2)
+                .And.ContainKey(target1.Id)
+                .And.ContainKey(target4.Id);
+        }
+
+        /// <summary>
+        /// Получение ответа, существует ли такой компонент по Id, возвращает false
+        /// </summary>
+        [Fact]
+        public async Task AnyByIdComponentFalse()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await componentsReadRepository.AnyByIdAsync(id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .BeFalse();
+        }
+
+        /// <summary>
+        /// Получение ответа, существует ли такой компонент по Id, возвращает true
+        /// </summary>
+        [Fact]
+        public async Task AnyByIdComponentTrue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Component();
+            await Context.Components.AddAsync(target);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await componentsReadRepository.AnyByIdAsync(target.Id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .BeTrue();
+        }
+    }
+}
---
+++ Accessories_PC_Nik.Repositories.Tests/Tests/ServiceReadRepositoryTests.cs
@@ -0,0 +1,208 @@
+﻿using Accessories_PC_Nik.Context.Tests;
+using Accessories_PC_Nik.Repositories.Contracts.Interface;
+using Accessories_PC_Nik.Repositories.Implementations;
+using FluentAssertions;
+using Xunit;
+
+namespace Accessories_PC_Nik.Repositories.Tests.Tests
+{
+    public class ServiceReadRepositoryTests : AccessoriesContextInMemory
+    {
+        private readonly IServicesReadRepository servicesReadRepository;
+
+        public ServiceReadRepositoryTests()
+        {
+            servicesReadRepository = new ServicesReadRepository(Reader);
+        }
+
+        /// <summary>
+        /// Возвращает пустой список сервисов
+        /// </summary>
+        [Fact]
+        public async Task GetAllServiceEmpty()
+        {
+            // Act
+            var result = await servicesReadRepository.GetAllAsync(CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+        }
+
+        /// <summary>
+        /// Возвращает список сервисов
+        /// </summary>
+        [Fact]
+        public async Task GetAllServicesValue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+            await Context.Services.AddRangeAsync(target,
+                TestDataGenerator.Service(x => x.DeletedAt = DateTimeOffset.UtcNow));
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await servicesReadRepository.GetAllAsync(CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.HaveCount(1)
+                .And.ContainSingle(x => x.Id == target.Id);
+        }
+
+        /// <summary>
+        /// Получение сервиса по идентификатору и возвращает null
+        /// </summary>
+        [Fact]
+        public async Task GetByIdServiceNull()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await servicesReadRepository.GetByIdAsync(id, CancellationToken);
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Получение сервиса по идентификатору и возвращает данные
+        /// </summary>
+        [Fact]
+        public async Task GetByIdServiceValue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+            await Context.Services.AddAsync(target);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await servicesReadRepository.GetByIdAsync(target.Id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEquivalentTo(target);
+        }
+
+        /// <summary>
+        /// Получение списка сервисов по идентификаторам и возвращает пустую коллекцию
+        /// </summary>
+        [Fact]
+        public async Task GetByIdsServicesEmpty()
+        {
+            //Arrange
+            var id1 = Guid.NewGuid();
+            var id2 = Guid.NewGuid();
+            var id3 = Guid.NewGuid();
+
+            // Act
+            var result = await servicesReadRepository.GetByIdsAsync(new[] { id1, id2, id3 }, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+        }
+
+        /// <summary>
+        /// Получение списка сервисов по идентификаторам и возвращает данные
+        /// </summary>
+        [Fact]
+        public async Task GetByIdsServicesValue()
+        {
+            //Arrange
+            var target1 = TestDataGenerator.Service();
+            var target2 = TestDataGenerator.Service(x => x.DeletedAt = DateTimeOffset.UtcNow);
+            var target3 = TestDataGenerator.Service();
+            var target4 = TestDataGenerator.Service();
+            await Context.Services.AddRangeAsync(target1, target2, target3, target4);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await servicesReadRepository.GetByIdsAsync(new[] { target1.Id, target2.Id, target4.Id }, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.HaveCount(2)
+                .And.ContainKey(target1.Id)
+                .And.ContainKey(target4.Id);
+        }
+
+        /// <summary>
+        /// Получение ответа, существует ли такой сервис по Id, возвращает false
+        /// </summary>
+        [Fact]
+        public async Task AnyByIdServiceFalse()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await servicesReadRepository.AnyByIdAsync(id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .BeFalse();
+        }
+
+        /// <summary>
+        /// Получение ответа, существует ли такой сервис по Id, возвращает true
+        /// </summary>
+        [Fact]
+        public async Task AnyByIdServiceTrue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+            await Context.Services.AddAsync(target);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await servicesReadRepository.AnyByIdAsync(target.Id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .BeTrue();
+        }
+
+        /// <summary>
+        /// Получение ответа, существует ли такое имя - не существует
+        /// </summary>
+        [Fact]
+        public async Task AnyByNameServiceFalse()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+
+            // Act
+            var result = await servicesReadRepository.AnyByNameAsync(target.Name, CancellationToken);
+
+            // Assert
+            result.Should()
+                .BeFalse();
+        }
+
+        /// <summary>
+        /// Получение ответа, существует ли такое имя - существует
+        /// </summary>
+        [Fact]
+        public async Task AnyByNameServiceTrue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+            await Context.Services.AddAsync(target);
+            await UnitOfWork.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await servicesReadRepository.AnyByNameAsync(target.Name, CancellationToken);
+
+            // Assert
+            result.Should()
+                .BeTrue();
+        }
+    }
+}
